db63e2fc3d4a850788f649f96e6f9cbf734f5a97
SomeKittens <rkoutnik@gmail.com>
2014-03-15 15:47:14 -0700

    moved plugins

--- source/plugins/vendetta.js
+++
@@ -1,6 +0,0 @@
-IO.register( 'input', function ( msgObj ) {
-	if ( msgObj.user_id === 1386886 && Math.random() < 0.005 ) {
-		bot.adapter.out.add(
-			bot.adapter.reply(msgObj.user_name) + ' The Game' );
-	}
-});
